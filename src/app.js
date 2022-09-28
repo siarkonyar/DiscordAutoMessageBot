b1408487aa4300dc2a2c76e39eadf08a6997a405
siarkonyar <siarkonyar@gmail.com>
2022-09-29 00:35:08 +0300

    made commands more general and made code more clean

--- src/app.js
+++ src/app.js
@@ -1,76 +1,82 @@
-const { Client,  Intents } = require("discord.js");
+const { Client, Intents } = require("discord.js");
 
 var siarNotlar = [];
 var baranNotlar = [];
 var ciwanNotlar = [];
 var bahatNotlar = [];
 
-function showList(array){
-    if(!array.isArray()){
+function showList(array) {
+    if (!array.isArray()) {
         message.reply("array gir!");
-    }else{
-        for(let i=0; i<array.length; i++){
-            let line = i+1;
-            message.reply(line+ "." +array[i]);
+    } else {
+        for (let i = 0; i < array.length; i++) {
+        let line = i + 1;
+        message.reply(line + ". " + array[i]);
         }
     }
+}
+
+function commands(array) {
+    var command = message.content.split(" ");
+    var commandRequest = commandRequest[0];
+    if (commandRequest == "//add") {
+        let note = "";
+        for (let i = 1; i < command.length; i++) {
+            note.concat(command[i]);
+        }
+        array.push(note);
+
+        message.reply("notlara eklendi");
+
+        showList(array);
+    } else if (commandRequest == "//remove") {
+        let removedIndex = command[1] - 1;
+        if (typeof command[1] !== "number" && !Number.isNaN(command[1])) {
+            message.reply("ne");
+        } else {
+            array.splice(removedIndex, 1);
+
+            message.reply("notlardan silindi.");
+            showList(array);
+        }
+    } else if (commandRequest == "//show") {
+        showList(array);
+    }
+    //message.channel.send("content")
 }
 
 const client = new Client({
     intents: ["DIRECT_MESSAGES", "GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"],
     presence: {
         status: "online",
-        activities: [{
+        activities: [
+        {
             name: "Arabalar 1",
-            type: "WATCHING"
-        }]
+            type: "WATCHING",
+        },
+        ],
     },
 });
 
-client.on('ready', () => { 
+client.on("ready", () => {
     console.log(`Launched as a bot: ${client.user.tag}!`);
 });
 
-client.on('messageCreate', message => {
-	if (message.author.id === process.env.USER_ID) {
-		message.reply(process.env.MESSAGE);
-        //message.channel.send("content")
-	}
+client.on("messageCreate", (message) => {
+    if (message.author.id === process.env.SIAR_ID) {
+        commands(siarNotlar);
+    }else if(message.author.id === process.env.BAHAT_ID){
+        commands(bahatNotlar);
+    }else if(message.author.id === process.env.BARAN_ID){
+        commands(baranNotlar);
+    }else if(message.author.id === process.env.CIWAN_ID){
+        commands(ciwanNotlar);
+    }
 });
 
-client.on('messageCreate', message => {
-	if (message.author.id === process.env.SIAR_ID) {
-        var command = message.content.split(" ");
-        var commandRequest = commandRequest[0];
-        if(command == "//add"){
-
-            let note = "";
-            for(let i=1; i<command.length; i++){
-                note.concat(command[i]);
-            }
-            siarNotlar.push(note);
-
-            message.reply("Şiar'ın notlarına eklendi");
-
-            showList(siarNotlar);
-        }else if(command == "//remove"){
-
-            let removedIndex = command[1]+1;
-            if(typeof command[1] !== 'number' && !Number.isNaN(command[1])){
-                message.reply("ne");
-            }else{
-                siarNotlar.splice(removedIndex, 1);
-
-                message.reply("Şiar'ın notlarindan silindi.");
-                showList(siarNotlar);
-            }
-        }
-        else if(command == "//show"){
-
-            showList(siarNotlar);
-        }
-        //message.channel.send("content")
-	}
+client.on("messageCreate", (message) => {
+    if (message.author.id === process.env.SIAR_ID) {
+    }
 });
 
 client.login(process.env.DJS_TOKEN);
