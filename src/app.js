b9609a43b1f656d99d9289641a89b9390088ae36
siarkonyar <siarkonyar@gmail.com>
2022-09-29 00:30:58 +0300

    added commands

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 const { Client,  Intents } = require("discord.js");
 
+var siarNotlar = [];
+var baranNotlar = [];
+var ciwanNotlar = [];
+var bahatNotlar = [];
+
+function showList(array){
+    if(!array.isArray()){
+        message.reply("array gir!");
+    }else{
+        for(let i=0; i<array.length; i++){
+            let line = i+1;
+            message.reply(line+ "." +array[i]);
+        }
+    }
+}
+
 const client = new Client({
     intents: ["DIRECT_MESSAGES", "GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"],
     presence: {
@@ -18,6 +34,41 @@ client.on('ready', () => {
 client.on('messageCreate', message => {
 	if (message.author.id === process.env.USER_ID) {
 		message.reply(process.env.MESSAGE);
+        //message.channel.send("content")
+	}
+});
+
+client.on('messageCreate', message => {
+	if (message.author.id === process.env.SIAR_ID) {
+        var command = message.content.split(" ");
+        var commandRequest = commandRequest[0];
+        if(command == "//add"){
+
+            let note = "";
+            for(let i=1; i<command.length; i++){
+                note.concat(command[i]);
+            }
+            siarNotlar.push(note);
+
+            message.reply("Şiar'ın notlarına eklendi");
+
+            showList(siarNotlar);
+        }else if(command == "//remove"){
+
+            let removedIndex = command[1]+1;
+            if(typeof command[1] !== 'number' && !Number.isNaN(command[1])){
+                message.reply("ne");
+            }else{
+                siarNotlar.splice(removedIndex, 1);
+
+                message.reply("Şiar'ın notlarindan silindi.");
+                showList(siarNotlar);
+            }
+        }
+        else if(command == "//show"){
+
+            showList(siarNotlar);
+        }
         //message.channel.send("content")
 	}
 });
