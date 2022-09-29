163f59f4921ba047bb7e8469e74bdd49dc8d667b
siarkonyar <siarkonyar@gmail.com>
2022-09-29 20:59:46 +0300

    trying to fix the code

--- src/app.js
+++ src/app.js
@@ -5,46 +5,6 @@ var baranNotlar = [];
 var ciwanNotlar = [];
 var bahatNotlar = [];
 
-function showList(array) {
-    if (!array.isArray()) {
-        message.reply('array gir!');
-    } else {
-        for (let i = 0; i < array.length; i++) {
-        let line = i + 1;
-        message.reply(line + '. ' + array[i]);
-        }
-    }
-}
-
-function commands(array, message) {
-    const command = message.content.trim().split(' ');
-    const commandRequest = command[0];
-    if (commandRequest === '//add') {
-        let note = '';
-        for (let i = 1; i < command.length; i++) {
-            note.concat(command[i]);
-        }
-        array.push(note);
-
-        message.reply('notlara eklendi');
-
-        showList(array);
-    } else if (commandRequest === '//remove') {
-        let removedIndex = command[1] - 1;
-        if (typeof command[1] !== 'number' && !Number.isNaN(command[1])) {
-            message.reply('ne');
-        } else {
-            array.splice(removedIndex, 1);
-
-            message.reply('notlardan silindi.');
-            showList(array);
-        }
-    } else if (commandRequest === '//show') {
-        showList(array);
-    }
-    //message.channel.send("content")
-}
-
 const client = new Client({
     intents: ["DIRECT_MESSAGES", "GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"],
     presence: {
@@ -62,16 +22,94 @@ client.on("ready", () => {
     console.log(`Launched as a bot: ${client.user.tag}!`);
 });
 
+/* function showList(array, message) {
+    if (!array.isArray()) {
+        message.reply('array gir!');
+    } else {
+        for (let i = 0; i < array.length; i++) {
+        let line = i + 1;
+        message.reply(line + '. ' + array[i]);
+        }
+    }
+}
+
+function commands(array, message) {
+    let command = message.content.trim().split(' ');
+    let commandRequest = command[0];
+    if (commandRequest === '//add') {
+        let note = '';
+        for (let i = 1; i < command.length; i++) {
+            note.concat(command[i]);
+        }
+        array.push(note);
+
+        message.reply('notlara eklendi');
+
+        showList(array, message);
+    } else if (commandRequest === '//remove') {
+        let removedIndex = command[1] - 1;
+        if (typeof command[1] !== 'number') {
+            message.reply('ne');
+        } else {
+            array.splice(removedIndex, 1);
+
+            message.reply('notlardan silindi.');
+            showList(array, message);
+        }
+    } else if (commandRequest === '//show') {
+        showList(array, message);
+    }
+    //message.channel.send("content")
+} */
+
 client.on("messageCreate", (message) => {
-    if (message.author.id === process.env.SIAR_ID) {
-        commands(siarNotlar, message);
+        if (message.author.id === process.env.SIAR_ID) {
+
+            let command = message.content.trim().split(' ');
+            let commandRequest = command[0];
+            if (commandRequest === '//add') {
+                let note = '';
+                for (let i = 1; i < command.length; i++) {
+                    note.concat(command[i]);
+                }
+                siarNotlar.push(note);
+
+                message.reply('notlara eklendi');
+
+                for (let i = 0; i < siarNotlar.length; i++) {
+                    let line = i + 1;
+                    message.reply(line + '. ' + siarNotlar[i]);
+                }
+
+            } else if (commandRequest === '//remove') {
+                let removedIndex = command[1] - 1;
+                if (typeof command[1] !== 'number') {
+                    message.reply('ne');
+                } else {
+                    siarNotlar.splice(removedIndex, 1);
+
+                    message.reply('notlardan silindi.');
+
+                    for (let i = 0; i < siarNotlar.length; i++) {
+                        let line = i + 1;
+                        message.reply(line + '. ' + siarNotlar[i]);
+                    }
+                }
+            } else if (commandRequest === '//show') {
+                for (let i = 0; i < siarNotlar.length; i++) {
+                    let line = i + 1;
+                    message.reply(line + '. ' + siarNotlar[i]);
+                }
+            }
+        }
+        /* commands(siarNotlar, message);
     }else if(message.author.id === process.env.BAHAT_ID){
         commands(bahatNotlar, message);
     }else if(message.author.id === process.env.BARAN_ID){
         commands(baranNotlar, message);
     }else if(message.author.id === process.env.CIWAN_ID){
         commands(ciwanNotlar, message);
-    }
+    } */
 });
 
 client.login(process.env.DJS_TOKEN);
